ab6f2f0fda39d2706ac606405ec2ab364f01c807
SD123456789 <47637207+SD123456789@users.noreply.github.com>
2021-03-12 10:41:51 -0500

    adding functionality

--- IPsearchWithinACpolicy.py
+++ IPsearchWithinACpolicy.py
@@ -1,66 +1,164 @@
 #! /usr/bin/python3
 
 """
-IPsearchWithinACpolicy.py is a python script that makes use of the requests
-module to search within an Access Control Policy for a specified IP.
+File: IPsearchWithinACpolicy.py 
 
-To run the script use the following syntax:
-./IPsearchWithinACpolicy.py <IP address of server> <IP address you are looking for>
-python3 ./IPsearchWithinACpolicy.py <IP address of server> <IP address you are looking for>
+Usage: is a python script that makes use of the requests
+    module to search within an Access Control Policy for a specified IP.
+
+    ./IPsearchWithinACpolicy.py <username> <password> <IP address of FMC> <IP address you are looking for>
+    python3 ./IPsearchWithinACpolicy.py <username> <password> <IP address of FMC> <IP address you are looking for>
+
+Inputs: 
+    username,
+    password,
+    IP address of FMC,
+    IP address you are looking for
+
+Outputs:
+    Name of the Access Control Policy that contains the IP address (if it exists)
+
+Author: Sudhir H Desai <suddesai@cisco.com>
+
+
+License: BSD 3-Clause License
+
+Copyright (c) 2020, Cisco Systems, Inc. and/or its affiliates
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
+import argparse
+import json
+import pprint
 import requests
-import ipaddress
 import sys
 import os
-
+from collections import defaultdict
 from requests.models import HTTPError
-
-
-def sanitizeInput(inputs):
-	""" if there is more than one command line argument, exit """
-	if len(inputs) != 3:
-		print(f"Usage: {inputs[0]}  <IP address of FMC management interface>\n ")
-		sys.exit(1)
-
-	""" now that there are only two command line arguments, make sure both are an IP & return """	
-	try:
-		fmcIP = ipaddress.ip_address(inputs[1])
-		return fmcIP
-	except ValueError:
-	    print(f"address/netmask is invalid: {inputs[1]}\n")
-	    sys.exit(1)
-	except HTTPError:
-		print(f"address has no exposed API: {inputs[1]}\n")
-		sys.exit(1)
-	except:
-	    print(f"Usage: {inputs[0]}  <IP address of FTD management interface>\n")
-	    sys.exit(1)
-
+from requestToken import get_token
 
 
 def FMCexists(fmcIP):
-	""" make sure IP exists """
-	IP = str(fmcIP)
-	if (os.system(f"ping -c 1 -t 1 {IP}") != 0):
-		print("Please enter a useable IP address.\n")
-		sys.exit(1)
+    # make sure IP exists
+    IP = str(fmcIP)
+    if (os.system(f"ping -c 1 -t 1 {IP}") != 0):
+        print("Please enter a useable IP address.\n")
+        sys.exit(1)
 
-	""" getting valid versions using the built-in module exceptions to handle errors """
-	r = requests.get('https://{}/api/versions'.format(str(IPaddr)), verify=False)
+    # lets disable the certificate warning first (this is NOT advised in prod)
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-	try:
-		r.raise_for_status()
-	except:
-		print(f"The IP address at {IPaddr} has no exposed API and has returned a {r.status_code} error.")
-		exit(1)
+    # getting valid versions using the built-in module exceptions to handle errors
+    try:
+        r = requests.get('https://{}//api/fmc_platform/v1/info/serverversion'.format(str(fmcIP)), verify=False)
 
-	return r,IPaddr
+    except:
+        print(f"The IP address at {fmcIP} has no exposed API and has returned a {r.status_code} error.")
+        exit(1)
+
+    return fmcIP
+
+
+def getFunction(fmcIP, apiPath, authHeader):
+
+    # now to try and GET our list of whatever
+    try:
+        r = requests.get(f"https://{fmcIP}/api/fmc_config/v1/domain/{authHeader['DOMAIN_UUID']}/object{apiPath}", headers=authHeader, 
+            verify=False) # always verify the SSL cert in prod!
+    except requests.exceptions.HTTPError as errh:
+        raise SystemExit(errh)
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+
+
+    # if it worked, we will have received a list of whatever so return it
+    try:
+        return r.json()
+    except Exception as err:
+        raise SystemExit(err)
 
 
 if __name__ == "__main__":
-	try:
-		whichVersions, IP = FMCexists(sanitizeInput(sys.argv))
-		
-	except:
-		exit(1)
+    # first set up the command line arguments and parse them
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("username", type=str, help ="API username")
+    parser.add_argument("password", type=str, help="password of API user")
+    parser.add_argument("ip_of_fmc", type=str, help="IP of FMC")
+    parser.add_argument("ip_to_search", type=str, help="IP that is being searched for")
+    args = parser.parse_args()
+
+
+    # set needed variables to generate a token
+    user = args.username
+    passwd = args.password
+    fmcIP = args.ip_of_fmc
+    queriedIP = args.ip_to_search
+
+
+    # then make sure that the FMC IP is valid
+    try:
+        fmcIP = FMCexists(fmcIP)
+    except:
+        exit(1)
+
+    
+    # next get a valid token
+    apiPath = "/api/fmc_platform/v1/auth/generatetoken"
+    authHeader = get_token(fmcIP, apiPath, user, passwd)
+
+
+    # get all networkgroups from the FMC
+    # set the path to the network groups and expand them for values
+    apiPath = f"/networkgroups?expanded=true"
+    networkObjectGroups = getFunction(fmcIP, apiPath, authHeader)
+
+
+    # next grab all network objects from the FMC
+    # set the path to get network objects and expand them for values
+    apiPath = f"/networks?expanded=true"
+    networkObjects = getFunction(fmcIP, apiPath, authHeader)
+
+
+    # then pull all IP addresses from each network object and object group
+    # saving them in a dict of the form object:[list, of, IPs]
+    netObjDict = defaultdict(list)
+    for networkObjectGroup in networkObjectGroups['items']:
+        if networkObjectGroup.get('literals') != None:
+            if len(networkObjectGroup['literals']) > 1:
+                for literal in networkObjectGroup['literals']:
+                    netObjDict[networkObjectGroup['id']].append(literal['value'])
+            else:
+                netObjDict[networkObjectGroup['id']] = networkObjectGroup['literals'][0]['value']
+
+    for networkObject in networkObjects['items']:
+        netObjDict[networkObject['id']] = networkObject['value']
+
+
+    # next find the IP we are looking for from within that header
+    pprint.pprint(netObjDict)
---
+++ requestToken.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+"""
+File: requestToken.py
+Inputs: none
+Outputs: print both access token and refresh token to screen
+
+To use this file as a standalone script the username, password, & FMC IP
+will need to be populated in the __main__ section below.
+
+Author: Sudhir H Desai <suddesai@cisco.com>
+
+
+License: BSD 3-Clause License
+
+Copyright (c) 2020, Cisco Systems, Inc. and/or its affiliates
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+# include the necessary modules
+import argparse
+import requests
+
+"""
+function: get_token(fmcIP, path, username, password)
+use: generates a list of necessary headers to be included with all 
+    subsequent requests
+
+inputs: IP of FMC, path to API, API user, API password
+returns: access token, refresh token, domain uuid
+"""
+def get_token(fmcIP, path, username, password):
+    # lets disable the certificate warning first (this is NOT advised in prod)
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    # send request with a try/catch block to handle errors safely
+    try:
+        r = requests.post(f"https://{fmcIP}/{path}", auth=(f"{username}", 
+            f"{password}"), verify=False) # always verify the SSL cert in prod!
+    except requests.exceptions.HTTPError as errh:
+        raise SystemExit(errh)
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+
+    # return the request token by identifying which key:value pairs we need
+    required_headers = ('X-auth-access-token', 'X-auth-refresh-token', 'DOMAIN_UUID')
+    result = {key: r.headers.get(key) for key in required_headers}
+    return result
+
+"""
+function: refresh_token(fmcIP, path, access token, refresh token)
+use: updates the access and refresh tokens of the passed-in header bundle
+
+inputs: IP of FMC, path to API, access token, refresh token
+returns: none
+"""
+def refresh_token(fmcIP, path, header):
+    # lets disable the certificate warning first (this is NOT advised in prod)
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+    # send request with a try/catch block to handle errors safely
+    try:
+        r = requests.post(f"https://{fmcIP}/{path}", headers=header, 
+            verify=False) # always verify the SSL cert in prod!
+    except requests.exceptions.HTTPError as errh:
+        raise SystemExit(errh)
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
+
+     # update the request token
+    header['X-auth-access-token'] = r.headers.get('X-auth-access-token')
+    header['X-auth-refresh-token'] = r.headers.get('X-auth-refresh-token')
+
+    # pass since not returning anything
+    pass
+
+
+# if used as a stand-alone script, run the following
+if __name__ == "__main__":
+    # first set up the command line arguments and parse them
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("username", type=str, help ="API username")
+    parser.add_argument("password", type=str, help="password of API user")
+    parser.add_argument("ip_address", type=str, help="IP of FMC")
+    args = parser.parse_args()
+
+    # set needed variables to generate a token
+    u = args.username
+    p = args.password
+    ip = args.ip_address
+    path = "/api/fmc_platform/v1/auth/generatetoken"
+
+    # call the token generating function and populate our header
+    header = get_token(ip, path, u, p)
+
+    # print the access token, refresh token, and domain uuid to the cli
+    print(f"The Access Token received is: {header.get('X-auth-access-token')}")
+    print(f"The Refresh Token received is: {header.get('X-auth-refresh-token')}")
+    print(f"The DOMAIN_UUID is: {header.get('DOMAIN_UUID')}")
+
+    # set the needed variables to refresh a token - only the new path, really
+    path = "/api/fmc_platform/v1/auth/refreshtoken"
+
+    # call the token refreshing function
+    refresh_token(ip, path, header)
+
+    # print the new access token and refresh token to the cli
+    print(f"The refreshed Access Token received is: {header.get('X-auth-access-token')}")
+    print(f"The refreshed Refresh Token received is: {header.get('X-auth-refresh-token')}")
